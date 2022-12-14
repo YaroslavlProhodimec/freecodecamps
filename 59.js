c7cb3e22f8dbf85d3face3f97569dd9661f8d2bd
Yaroslav Myratov <yaroslav.muratov.97@inbox.ru>
2022-12-14 13:37:27 +0300

    privetiki

---
+++ 59.js
@@ -0,0 +1,19 @@
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "Yes, it's true";
+    }
+    return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(myCondition) {
+
+// Only change code below this line.
+    if(myCondition) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+
+// Only change code above this line.
+
+}
